9f4c8465fc033ac1c2d7c14bed63174765f4003b
Michal_T <michal.trostowiecki@gmail.com>
2024-01-14 18:13:41 +0000

    Add javascript to browse through the history search

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -39,22 +39,28 @@ function getWeather(city) {
             return response.json();
         })
         .then(function(data) {
-            console.log(data)
             
             // Convert time stamp to current date after
             let unixTimeStamp = data.list[0].dt
             let date = dayjs.unix(unixTimeStamp).format("DD-MM-YYYY");
 
+            // Get the weather icon to display
+            let weatherIcon = data.list[0].weather[0].icon
+            let iconSrc = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+
             // Create html elements with data from API
+            let todayTitle = $("<div>");
+            let icon = $("<img>").attr("src", iconSrc);
             cityName = $("<h2>").text(data.city.name + " " + date);
             let temp = data.list[0].main.temp;
             temperature = $("<p>").text("Temp: " + temp.toFixed(1) + " °C");
             let wind = $("<p>").text("Wind: " + data.list[0].wind.speed + " KPH")
             let humidity = $("<p>").text("Humidity: " + data.list[0].main.humidity + "%")
-
-
+            
+            
             // Append all html elements to div displaying today's weather
-            $("#today").append(cityName, temperature, wind, humidity);
+            todayTitle.append(cityName, icon)
+            $("#today").append(todayTitle, temperature, wind, humidity);
             
             display5Days(data.list);
         })
@@ -70,13 +76,22 @@ $("#search-button").on("click", function(event) {
     
     event.preventDefault();
 
+    //Clear today's weather and forecast
+    $("#today").empty();
+    $("#forecast").empty();
+
+
     // Get value from user input - city name
     let searchInput = $("#search-input").val()
     getWeather(searchInput)
 
+    // Generate buttons with user searches
+    let historyBtn = $("<button>").text(searchInput);
+    $("#history").prepend(historyBtn);
+
 })
 
-
+// Function to display 5 days forecast
 function display5Days(data) {
     let groupedData = {};
 
@@ -97,9 +112,10 @@ function display5Days(data) {
 
     });
 
+    // Looping through dates
     Object.keys(groupedData).forEach(function(date) {
         let items = groupedData[date];
-        console.log(items)
+        
 
         let totalTemp = 0;
         let totalWind = 0;
@@ -107,6 +123,7 @@ function display5Days(data) {
 
         // Variable to store the codes for the weather icons, to see which one occurs the most often
         let iconCode = [];
+
         
         // Looping through each item to get sum up all data
         for (let i = 0; i < items.length; i++) {
@@ -115,6 +132,8 @@ function display5Days(data) {
             totalHumidity += items[i].main.humidity;
             iconCode.push(items[i].weather[0].icon);
         }
+        // Get most frequent icon for the weather to display
+        let weatherIcon = findMostFrequentIcon(iconCode);
 
         // Assigning average temp, wind, humidity for a day
         let avgTemp = (totalTemp / items.length).toFixed(1);
@@ -123,7 +142,7 @@ function display5Days(data) {
 
         // Manipulating DOM to display weather
         let dayDate = $("<h3>").text(date);
-        let icon;
+        let icon = $("<img>").attr("src", `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`)
         let dayTemp = $("<p>").text("Temp: " + avgTemp + " °C");
         let daywind = $("<p>").text("Wind: " + avgWind + " KPH");
         let dayHumidity = $("<p>").text("Humidity: " + avgHumidity + "%");
@@ -133,38 +152,60 @@ function display5Days(data) {
 
         $("#forecast").append(dayContainer);
         
-        let weatherIcon = findMostFrequentIcon(iconCode);
-        console.log(weatherIcon);
+        
+        
     })
 
 }
 
-// Function to find the most frequent icon in "iconCode" array
+// Function to find the most frequent icon in "iconCode" array for a day
 function findMostFrequentIcon(array) {
 
-    // Variables to store items and it's frequency
-    let frequency = {};
-    let maxFrequency = 0;
-    let mostFrequentIcon = null;
-
-
-    // Loop through to find out frequency of each item
-    for (let i = 0; i < array.length; i++) {
-        let item = array[i];
-
-        if (frequency[item]) {
-            frequency[item]++;
-        } else {
-            frequency[item] = 1;
-        }
-
-        // Check if the current item is the most frequent one so far
-        if (frequency[item] > maxFrequency) {
-            maxFrequency = frequency[item];
-            mostFrequentIcon = item
-        }
+    let getMostFrequent = function(arr) {
         
+        // Variables to store items and it's frequency
+        let frequency = {};
+        let maxFrequency = 0;
+        let mostFrequentIcon = null;
+        
+        // Loop through to find out frequency of each item
+        for (let i = 0; i < arr.length; i++ ) {
+            let item = arr[i];
+            frequency[item] = (frequency[item] || 0) + 1;
+            
+            if (frequency[item] > maxFrequency) {
+                maxFrequency = frequency[item];
+                mostFrequentIcon = item;
+            }
+        }
+        return mostFrequentIcon;
+        
+    };
+
+    //In API icons code for day is eg. "03d", "04d"
+    let dayIcons = array.filter(function(icon) {
+        return icon.endsWith('d');
+    });
+
+    let mostFrequentDayIcon = getMostFrequent(dayIcons);
+
+    // If no weather icons for day use the one for night which end with eg. "03n"
+    if (!mostFrequentDayIcon && array.length > 0) {
+        mostFrequentDayIcon = getMostFrequent(array);
     }
 
-    return mostFrequentIcon;
+    return mostFrequentDayIcon;
 }
+
+// Function to display weather when User click button from search history
+$("#history").on("click", "button", function() {
+    let cityBtn = $(this).text();
+    
+    // Clear existing weather on a screen
+    $("#today").empty();
+    $("#forecast").empty();
+
+    // Make an API call when button is clicked with the coresponding name of a city
+    getWeather(cityBtn);
+
+} )
